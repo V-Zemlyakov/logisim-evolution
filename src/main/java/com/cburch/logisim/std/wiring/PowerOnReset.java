7b2b89e40247f26228ac14bbb50a315f6369603d
Zemlyakov <zemlyakov@gmail.com>
2026-09-06 18:13:14 +0300

    feat: add unit selection and dynamic duration limits to PowerOnReset component

--- src/main/java/com/cburch/logisim/std/wiring/PowerOnReset.java
+++ src/main/java/com/cburch/logisim/std/wiring/PowerOnReset.java
@@ -12,6 +12,7 @@ package com.cburch.logisim.std.wiring;
 import static com.cburch.logisim.std.Strings.S;
 
 import com.cburch.logisim.circuit.Simulator;
+import com.cburch.logisim.data.AbstractAttributeSet;
 import com.cburch.logisim.data.Attribute;
 import com.cburch.logisim.data.AttributeOption;
 import com.cburch.logisim.data.AttributeSet;
@@ -20,6 +21,7 @@ import com.cburch.logisim.data.BitWidth;
 import com.cburch.logisim.data.Bounds;
 import com.cburch.logisim.data.Direction;
 import com.cburch.logisim.data.Value;
+import com.cburch.logisim.gui.generic.ComboBox;
 import com.cburch.logisim.instance.Instance;
 import com.cburch.logisim.instance.InstanceComponent;
 import com.cburch.logisim.instance.InstanceData;
@@ -31,14 +33,20 @@ import com.cburch.logisim.instance.Port;
 import com.cburch.logisim.instance.StdAttr;
 import com.cburch.logisim.prefs.AppPreferences;
 import com.cburch.logisim.util.GraphicsUtil;
+import com.cburch.logisim.util.StringGetter;
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.JTextField;
 import javax.swing.Timer;
 
 public class PowerOnReset extends InstanceFactory {
@@ -68,6 +76,83 @@ public class PowerOnReset extends InstanceFactory {
       Attributes.forOption(
           "porTransition", S.getter("porTransition"), new AttributeOption[] {HTOL, LTOH});
 
+  public static final AttributeOption UNIT_SECONDS =
+      new AttributeOption(1, S.getter("realTimeClockSeconds"));
+  public static final AttributeOption UNIT_MILLISECONDS =
+      new AttributeOption(2, S.getter("realTimeClockMilliseconds"));
+  public static final Attribute<AttributeOption> PORUNIT =
+      Attributes.forOption(
+          "porUnit", S.getter("porDurationUnit"), new AttributeOption[] {UNIT_SECONDS, UNIT_MILLISECONDS});
+
+  private static final Integer[] PRESETS_SEC = new Integer[] {1, 2, 3, 4, 5, 10};
+  private static final Integer[] PRESETS_MS = new Integer[] {10, 20, 50, 100, 200, 500, 1000, 2000, 5000};
+
+  private static class PORDurationAttribute extends Attribute<Integer> {
+    private final AttributeSet attrs;
+
+    public PORDurationAttribute(String name, StringGetter disp, AttributeSet attrs) {
+      super(name, disp);
+      this.attrs = attrs;
+    }
+
+    @Override
+    public Component getCellEditor(Window source, Integer value) {
+      final var isMs = attrs != null && attrs.getValue(PORUNIT) == UNIT_MILLISECONDS;
+      final var presets = isMs ? PRESETS_MS : PRESETS_SEC;
+      final var combo = new ComboBox<>(presets);
+      combo.setEditable(true);
+
+      if (value != null) {
+        combo.setSelectedItem(value);
+        final var editorComp = combo.getEditor().getEditorComponent();
+        if (editorComp instanceof JTextField field) {
+          field.setText(value.toString());
+        }
+      }
+      return combo;
+    }
+
+    @Override
+    public Integer parse(String value) {
+      try {
+        final var ret = Integer.parseInt(value.trim());
+        final var isMs = attrs != null && attrs.getValue(PORUNIT) == UNIT_MILLISECONDS;
+        final var min = 1;
+        final var max = isMs ? 60000 : 60;
+        if (ret < min) {
+          throw new PORValidationException(S.get("durationSmallMessage", "" + min));
+        } else if (ret > max) {
+          throw new PORValidationException(S.get("durationLargeMessage", "" + max));
+        }
+        return ret;
+      } catch (PORValidationException e) {
+        throw e;
+      } catch (NumberFormatException e) {
+        throw new NumberFormatException(S.get("freqInvalidMessage"));
+      }
+    }
+
+    @Override
+    public String toDisplayString(Integer value) {
+      if (value == null) return "";
+      if (attrs != null && attrs.getValue(PORUNIT) == UNIT_MILLISECONDS) {
+        return S.get("PORDurationMsValue", value.toString());
+      } else {
+        if (value.equals(1)) {
+          return S.get("PORDurationOneValue");
+        } else {
+          return S.get("PORDurationValue", value.toString());
+        }
+      }
+    }
+  }
+
+  private static class PORValidationException extends NumberFormatException {
+    PORValidationException(String message) {
+      super(message);
+    }
+  }
+
   public static final PowerOnReset FACTORY = new PowerOnReset();
 
   public static class Poker extends InstancePoker {
@@ -78,24 +163,112 @@ public class PowerOnReset extends InstanceFactory {
     }
   }
 
+  private static class PowerOnResetAttributes extends AbstractAttributeSet {
+    private Direction facing = Direction.EAST;
+    private AttributeOption size = SIZE_WIDE;
+    private AttributeOption trans = HTOL;
+    private AttributeOption unit = UNIT_SECONDS;
+    private int duration = 2;
+
+    private final Attribute<Integer> durationAttr =
+        new PORDurationAttribute("PorHighDuration", S.getter("porHighAttr"), this);
+
+    private final List<Attribute<?>> attributes =
+        Arrays.asList(StdAttr.FACING, PORSIZE, PORTRANS, PORUNIT, durationAttr);
+
+    @Override
+    protected void copyInto(AbstractAttributeSet destObj) {
+      final var dest = (PowerOnResetAttributes) destObj;
+      dest.facing = this.facing;
+      dest.size = this.size;
+      dest.trans = this.trans;
+      dest.unit = this.unit;
+      dest.duration = this.duration;
+    }
+
+    @Override
+    public List<Attribute<?>> getAttributes() {
+      return attributes;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <V> V getValue(Attribute<V> attr) {
+      if (attr == StdAttr.FACING) return (V) facing;
+      if (attr == PORSIZE) return (V) size;
+      if (attr == PORTRANS) return (V) trans;
+      if (attr == PORUNIT) return (V) unit;
+      if (attr == durationAttr || "PorHighDuration".equals(attr.getName())) return (V) Integer.valueOf(duration);
+      return null;
+    }
+
+    @Override
+    public <V> void setValue(Attribute<V> attr, V value) {
+      if (attr == StdAttr.FACING) {
+        final var newFacing = (Direction) value;
+        if (!facing.equals(newFacing)) {
+          facing = newFacing;
+          fireAttributeValueChanged(attr, value, null);
+        }
+      } else if (attr == PORSIZE) {
+        final var newSize = (AttributeOption) value;
+        if (!size.equals(newSize)) {
+          size = newSize;
+          fireAttributeValueChanged(attr, value, null);
+        }
+      } else if (attr == PORTRANS) {
+        final var newTrans = (AttributeOption) value;
+        if (!trans.equals(newTrans)) {
+          trans = newTrans;
+          fireAttributeValueChanged(attr, value, null);
+        }
+      } else if (attr == PORUNIT) {
+        final var newUnit = (AttributeOption) value;
+        if (!unit.equals(newUnit)) {
+          final var oldUnit = unit;
+          unit = newUnit;
+          if (oldUnit == UNIT_SECONDS && newUnit == UNIT_MILLISECONDS) {
+            duration = Math.min(duration * 1000, 60000);
+            fireAttributeValueChanged(durationAttr, Integer.valueOf(duration), null);
+          } else if (oldUnit == UNIT_MILLISECONDS && newUnit == UNIT_SECONDS) {
+            duration = Math.max(1, (duration + 999) / 1000);
+            duration = Math.min(duration, 60);
+            fireAttributeValueChanged(durationAttr, Integer.valueOf(duration), null);
+          }
+          fireAttributeValueChanged(attr, value, null);
+        }
+      } else if (attr == durationAttr || "PorHighDuration".equals(attr.getName())) {
+        final var newDuration = (Integer) value;
+        if (duration != newDuration) {
+          duration = newDuration;
+          fireAttributeValueChanged(attr, value, null);
+        }
+      }
+    }
+
+    @Override
+    public <V> List<Attribute<?>> attributesMayAlsoBeChanged(Attribute<V> attr, V value) {
+      if (attr == PORUNIT) {
+        return List.of(durationAttr);
+      }
+      return null;
+    }
+
+    public int getDurationInMs() {
+      return unit == UNIT_MILLISECONDS ? duration : duration * 1000;
+    }
+  }
+
   public PowerOnReset() {
     super(_ID, S.getter("PowerOnResetComponent"));
-    setAttributes(
-        new Attribute[] {
-          StdAttr.FACING,
-          PORSIZE,
-          PORTRANS,
-          new DurationAttribute("PorHighDuration", S.getter("porHighAttr"), 1, 10, false),
-        },
-        new Object[] {
-          Direction.EAST,
-          SIZE_WIDE,
-          HTOL,
-          2,
-        });
     setFacingAttribute(StdAttr.FACING);
     setIconName("por.png");
     setInstancePoker(Poker.class);
+  }
+
+  @Override
+  public AttributeSet createAttributeSet() {
+    return new PowerOnResetAttributes();
   }
 
   private static class PORState implements InstanceData, Cloneable, ActionListener {
@@ -112,10 +285,23 @@ public class PowerOnReset extends InstanceFactory {
       value = true;
       component = state.getInstance().getComponent();
       simulator = state.getProject().getSimulator();
-      DurationAttribute attr =
-          (DurationAttribute) state.getAttributeSet().getAttribute("PorHighDuration");
-      duration = state.getAttributeValue(attr) * 1000;
+      updateParameters(state);
       tim = new Timer(duration, this);
+      state.setPort(0, Value.createKnown(BitWidth.ONE, tstart), 0);
+      tim.start();
+    }
+
+    private void updateParameters(InstanceState state) {
+      final var attrs = state.getAttributeSet();
+      if (attrs instanceof PowerOnResetAttributes porAttrs) {
+        duration = porAttrs.getDurationInMs();
+      } else {
+        // Backward compatibility
+        final var unit = attrs.getValue(PORUNIT);
+        final var val = (Integer) attrs.getValue(attrs.getAttribute("PorHighDuration"));
+        final var durationVal = val != null ? val : 2;
+        duration = (unit == UNIT_MILLISECONDS) ? durationVal : durationVal * 1000;
+      }
 
       if (state.getAttributeValue(PORTRANS) == LTOH) {
         tstart = 0;
@@ -124,8 +310,6 @@ public class PowerOnReset extends InstanceFactory {
         tstart = 1;
         tend = 0;
       }
-      state.setPort(0, Value.createKnown(BitWidth.ONE, tstart), 0);
-      tim.start();
     }
 
     public boolean getValue() {
@@ -146,19 +330,7 @@ public class PowerOnReset extends InstanceFactory {
         value = false;
       }
       value = true;
-
-      if (state.getAttributeValue(PORTRANS) == LTOH) {
-        tstart = 0;
-        tend = 1;
-      } else {
-        tstart = 1;
-        tend = 0;
-      }
-
-      DurationAttribute attr =
-          (DurationAttribute) state.getAttributeSet().getAttribute("PorHighDuration");
-      duration = state.getAttributeValue(attr) * 1000;
-
+      updateParameters(state);
       state.setPort(0, Value.createKnown(BitWidth.ONE, tstart), 0);
 
       tim.setInitialDelay(duration);
@@ -307,9 +479,8 @@ public class PowerOnReset extends InstanceFactory {
       g.drawLine(x2, y1, x2, y2);
       g.drawLine(x2, y2, x3, y2);
 
-      g.setColor(new Color(AppPreferences.COMPONENT_COLOR.get()));
-      String txt = S.get("PowerOnResetComponent");
-      g.drawString(txt, x + 2, y + offset - 1);
+      g.setColor(Color.BLACK);
+      g.drawString(_ID, x + 2, y + offset - 1);
     }
 
     painter.drawPorts();
